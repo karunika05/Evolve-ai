8fee772d97ecdebde81479f1fd2d09d79f8b08a8
karunika05 <chaudharykarunika@gmail.com>
2025-11-24 13:55:51 +0530

    evolve

---
+++ node-tutorial/index.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blueBright('Hello World!'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.black.bgGreen('This is a success message.'))
